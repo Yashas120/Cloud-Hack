968bd08e3e24e19932b9e0ac207304545bf34c20
Yashas120 <Yashas120@gmail.com>
2022-03-30 12:20:53 +0530

    improve Aesthetics

--- startup.sh
+++ startup.sh
@@ -1,24 +1,26 @@
-sudo eval $(minikube docker-env)
+sudo echo -e "\033[1;36mStartup Sequence Initiated\033[0m"
+eval $(minikube docker-env)
 docker build -t flask-app-image -f flask-app-image.dockerfile . 
 kubectl apply -f secret.yaml
 kubectl apply -f configmap.yaml
 kubectl apply -f services.yaml 
 nohup minikube tunnel -c & 
 kubectl apply -f deployments.yaml
-echo -e  "\033[32mStartup Completed\033[0m" 
+echo -e  "\033[32mLaunch Completed\033[0m" 
 sleep 2s
-# tput sc
 while [ true ] ; do
-# tput rc
-kubectl get all
-echo -e "\033[44mPress any key after all the pods are deployed\033[0m"
+val=$(kubectl get all)
+printf "%s \n\033[44mPress any key after all the pods are deployed\033[0m\n" "$val"
 read -t 3 -n 1
 if [ $? = 0 ] ; then
-exit ;
+kubectl get pods --selector=app=flaskapi -o name | head -n 1 | xargs -I{} kubectl exec {} -- /bin/bash -c "python3 /app/records.py"
+echo -e  "\033[32mPosts inserted\033[0m" 
+exit;
+
 else
 clear
 fi
 done
+echo -e  "\033[31mPosts not inserted\033[0m" 
 # sleep 20s
 # kubectl exec -it <pod-name> -- /bin/bash -c "python3 /app/records.py" 
-# kubectl get pods --selector=app=flaskapi -o name | head -n 1 | xargs -I{} kubectl exec {} -- /bin/bash -c "python3 /app/records.py"
